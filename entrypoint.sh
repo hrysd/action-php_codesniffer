b767443ef5996265beb7e4887f9e6730b06b36be
Hiroshi Yoshida <hrysd22@gmail.com>
2020-01-07 02:10:22 +0900

    Fix command

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,5 @@ cd "${GITHUB_WORKSPACE}" || exit
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-php phpcs.phar --standard=PSR12 --extensions=php --report=checkstyle -q \
+php /usr/local/bin/phpcs.phar --standard=PSR12 --extensions=php --report=checkstyle -q src \
   | reviewdog -name=PHP_CodeSniffer -f=checkstyle -reporter=github-pr-check -diff='git diff'
