6c84287522b4989af060943ec8d8c69a171d1c22
Hiroshi Yoshida <hrysd22@gmail.com>
2020-01-09 01:11:28 +0900

    Make more configurable

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,5 @@ cd "${GITHUB_WORKSPACE}" || exit
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-php /usr/local/bin/phpcs.phar --standard=PSR12 --extensions=php --report=checkstyle -q src \
-  | reviewdog -name=PHP_CodeSniffer -f=checkstyle -reporter=github-pr-check -diff='git diff'
+php /usr/local/bin/phpcs.phar --standard=PSR12 --extensions=php --report=checkstyle -q ${INPUT_TARGET_DIRECTORY} \
+  | reviewdog -name=PHP_CodeSniffer -f=checkstyle -reporter=${INPUT_REPORTER} -level=${INPUT_LEVEL} -diff='git diff'
