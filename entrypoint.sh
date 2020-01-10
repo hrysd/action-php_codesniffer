b61aae0d01a203d0daecdcd85a36393edac1c4db
Hiroshi Yoshida <hrysd22@gmail.com>
2020-01-10 22:14:10 +0900

    We can choose standard option

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,5 @@ cd "${GITHUB_WORKSPACE}" || exit
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-php /usr/local/bin/phpcs.phar --standard=PSR12 --extensions=php --report=checkstyle -q ${INPUT_TARGET_DIRECTORY} \
+php /usr/local/bin/phpcs.phar --standard=${INPUT_STANDARD} --extensions=php --report=checkstyle -q ${INPUT_TARGET_DIRECTORY} \
   | reviewdog -name=PHP_CodeSniffer -f=checkstyle -reporter=${INPUT_REPORTER} -level=${INPUT_LEVEL} -diff='git diff'
